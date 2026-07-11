dce321a7261f7e94fa17334718c1fb47b669e5ab
ken12121122-dotcom <ken12121122@gmail.com>
2026-07-11 19:00:46 +0800

    chore: bump Amin Vault cache to v0.5

--- amin-vault/sw.js
+++ amin-vault/sw.js
@@ -1,4 +1,4 @@
-const CACHE='amin-vault-v04';
+const CACHE='amin-vault-v05';
 const ASSETS=['./','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
