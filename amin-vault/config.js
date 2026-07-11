fe7cc18e18dc36958eec1c70273d0661398d686e
ken12121122-dotcom <ken12121122@gmail.com>
2026-07-11 18:40:58 +0800

    feat: add public Amin Vault client configuration

---
+++ amin-vault/config.js
@@ -0,0 +1,6 @@
+window.VAULT_CONFIG = Object.freeze({
+  url: "https://iqftezadvzugjcvuwmzw.supabase.co",
+  publishableKey: "sb_publishable__VvmKXxVxH2_4sMVnYSyZw_DoP-4of_",
+  workspaceSlug: "amin-universal-vault",
+  configVersion: "0.1.0"
+});
