82ce33e1a2434d10ee3e85cd5056fe5aaffd6ac9
ken12121122-dotcom <ken12121122@gmail.com>
2026-07-12 17:05:51 +0800

    test: verify portrait dashboard and all primary routes

--- android-native/app/src/androidTest/java/com/amin/pocketgba/ControlCenterActivityTest.java
+++ android-native/app/src/androidTest/java/com/amin/pocketgba/ControlCenterActivityTest.java
@@ -1,15 +1,28 @@
 package com.amin.pocketgba;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import android.app.Activity;
+import android.app.Instrumentation.ActivityResult;
+import android.content.pm.ActivityInfo;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,17 +34,47 @@ public final class ControlCenterActivityTest {
     public final ActivityScenarioRule<ControlCenterActivity> activityRule =
             new ActivityScenarioRule<>(ControlCenterActivity.class);
 
+    @Before
+    public void setUpIntents() {
+        Intents.init();
+    }
+
+    @After
+    public void releaseIntents() {
+        Intents.release();
+    }
+
     @Test
-    public void rendersVisibleStartupStatusAndNativeEntries() {
+    public void rendersSimplifiedPortraitDashboard() {
         activityRule.getScenario().onActivity(activity -> {
+            assertEquals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, activity.getRequestedOrientation());
             View root = activity.findViewById(android.R.id.content);
-            assertTrue(containsText(root, "原生控制台"));
-            assertTrue(containsText(root, "重新自動偵測"));
-            assertTrue(containsText(root, "進入 Pokémon GBA 遊戲庫"));
-            assertTrue(containsText(root, "開啟權限控制中心"));
-            assertTrue(containsText(root, "開啟原生 APK 更新中心"));
-            assertTrue(containsText(root, "這一包實際新增了什麼"));
+            assertTrue(containsText(root, "遊戲控制台"));
+            assertTrue(containsText(root, "開啟 GBA 遊戲庫"));
+            assertTrue(containsText(root, "目前狀態"));
+            assertTrue(containsText(root, "權限與裝置"));
+            assertTrue(containsText(root, "版本與更新"));
+            assertTrue(containsText(root, "顯示系統詳細資訊"));
         });
+    }
+
+    @Test
+    public void primaryCardsRouteToRealActivities() {
+        intending(hasComponent(MainActivity.class.getName()))
+                .respondWith(new ActivityResult(Activity.RESULT_OK, null));
+        intending(hasComponent(PermissionCenterActivity.class.getName()))
+                .respondWith(new ActivityResult(Activity.RESULT_OK, null));
+        intending(hasComponent(UpdateHubActivity.class.getName()))
+                .respondWith(new ActivityResult(Activity.RESULT_OK, null));
+
+        onView(withContentDescription("進入 Pokémon GBA 遊戲庫")).perform(click());
+        intended(hasComponent(MainActivity.class.getName()));
+
+        onView(withContentDescription("開啟權限控制中心")).perform(click());
+        intended(hasComponent(PermissionCenterActivity.class.getName()));
+
+        onView(withContentDescription("開啟原生 APK 更新中心")).perform(click());
+        intended(hasComponent(UpdateHubActivity.class.getName()));
     }
 
     private boolean containsText(View view, String target) {
