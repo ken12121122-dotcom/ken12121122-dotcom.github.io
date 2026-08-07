8605a7f3c9903d1dfd29d4d267019f6a16e4cc30
ken12121122-dotcom <ken12121122@gmail.com>
2026-08-07 08:36:52 +0800

    chore: clean system graph activity imports

--- android-native/app/src/main/java/com/amin/pocketgba/SystemGraphActivity.java
+++ android-native/app/src/main/java/com/amin/pocketgba/SystemGraphActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
